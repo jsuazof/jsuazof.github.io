701ebefba270b42ec45529cc45d5a1e31bb4f969
Jorge Suazo <jsuazof@gmail.com>
2025-08-10 20:33:17 -0400

    Fix Astro configuration for custom domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jsuazof.github.io',
-  // base: '/portfolio-cloud-architect', // Temporarily commented out for testing
   publicDir: 'public',
   integrations: [mdx(), /* sitemap(), */ tailwind()]
 });
