b0e737b0ccad655f94645e487a9d79c4a7e7ae4f
Jorge Suazo <jsuazof@gmail.com>
2025-08-13 19:01:55 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
@@ -8,9 +7,6 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/jsuazof.github.io/' : '/',
   integrations: [
     mdx(),
-    tailwind({
-      applyBaseStyles: false
-    }),
     sitemap()
   ],
   output: 'static',
